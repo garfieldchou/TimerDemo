6cd9faebce337f37d2229a8fba4a738fd2508f0d
garfield_chou <garfield_chou@asus.com>
2016-12-26 17:59:21 +0800

    Add Handler and Runable for timer test
    Handler
    .postDelay()
    .post()
    Runable

--- app/src/main/java/com/garfield_chou/timerdemo/MainActivity.java
+++ app/src/main/java/com/garfield_chou/timerdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.garfield_chou.timerdemo;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Runnable has run!", "a second must have passed...");
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.post(run);
     }
 }
